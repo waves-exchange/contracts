0306d855eb11284be28f15071ddc1cd3168d9006
im2620493 <108935058+im2620493@users.noreply.github.com>
2022-07-28 12:37:01 +0300

    wip(test): add test case for claimREADONLY method

--- test/referral/claimREADONLY.mjs
+++ test/referral/claimREADONLY.mjs
@@ -1,11 +1,11 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { address } from '@waves/ts-lib-crypto';
-import { invokeScript, nodeInteraction as ni } from '@waves/waves-transactions';
+import { invokeScript, libs, nodeInteraction as ni } from '@waves/waves-transactions';
 import { create } from '@waves/node-api-js';
 
 chai.use(chaiAsPromised);
-// const { expect } = chai;
+const { expect } = chai;
 
 const apiBase = process.env.API_NODE_URL;
 const chainId = 'R';
@@ -16,25 +16,83 @@ describe('referral: claimREADONLY.mjs', /** @this {MochaSuiteModified} */() => {
   it(
     'should successfully claimREADONLY',
     async function () {
-      const programName = '';
-      const referralAddress = '';
+      const programName = 'ReferralProgram';
+      const treasuryContract = address(this.accounts.treasury, chainId);
+      const implementationContract = address(this.accounts.implementation, chainId);
+      const referrerAddress = address(this.accounts.referrerAccount, chainId);
+      const referralAddress = address(this.accounts.referralAccount, chainId);
+      const referrerReward = 1e4;
+      const referralReward = 1e2;
+
+      const expectedNewClaimerClaimed = 0;
+
+      const bytes = libs.crypto.stringToBytes(
+        `${programName}:${referrerAddress}:${referralAddress}`,
+      );
+      const signature = libs.crypto.signBytes(this.accounts.backend, bytes);
 
       const referral = address(this.accounts.referral, chainId);
 
-      const claimREADONLYTx = invokeScript({
+      const createReferralProgramTx = invokeScript({
         dApp: referral,
         payment: [],
         call: {
-          function: 'claimREADONLY',
+          function: 'createReferralProgram',
           args: [
             { type: 'string', value: programName },
-            { type: 'string', value: referralAddress },
+            { type: 'string', value: treasuryContract },
+            { type: 'string', value: implementationContract },
+            { type: 'string', value: this.wxAssetId },
           ],
         },
         chainId,
       }, this.accounts.manager);
-      await api.transactions.broadcast(claimREADONLYTx, {});
-      await ni.waitForTx(claimREADONLYTx.id, { apiBase });
+      await api.transactions.broadcast(createReferralProgramTx, {});
+      await ni.waitForTx(createReferralProgramTx.id, { apiBase });
+
+      const createPairTx = invokeScript({
+        dApp: referral,
+        payment: [],
+        call: {
+          function: 'createPair',
+          args: [
+            { type: 'string', value: programName },
+            { type: 'string', value: referrerAddress },
+            { type: 'string', value: referralAddress },
+            {
+              type: 'binary',
+              value: `base64:${libs.crypto.base64Encode(libs.crypto.base58Decode(signature))}`,
+            },
+          ],
+        },
+        chainId,
+      }, this.accounts.manager);
+      await api.transactions.broadcast(createPairTx, {});
+      await ni.waitForTx(createPairTx.id, { apiBase });
+
+      const incUnclaimedTx = invokeScript({
+        dApp: referral,
+        payment: [],
+        call: {
+          function: 'incUnclaimed',
+          args: [
+            { type: 'string', value: programName },
+            { type: 'string', value: referralAddress },
+            { type: 'integer', value: referrerReward },
+            { type: 'integer', value: referralReward },
+          ],
+        },
+        chainId,
+      }, this.accounts.implementation);
+      await api.transactions.broadcast(incUnclaimedTx, {});
+      await ni.waitForTx(incUnclaimedTx.id, { apiBase });
+
+      const expectedResponse = { type: 'Int', value: expectedNewClaimerClaimed };
+
+      const expr = `claimREADONLY(\"${programName}\", \"${referralAddress}\")`; /* eslint-disable-line */
+      const response = await api.utils.fetchEvaluate(referral, expr);
+
+      expect(response.result.value._2).to.eql(expectedResponse); /* eslint-disable-line */
     },
   );
 });
