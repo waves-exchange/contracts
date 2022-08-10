d32c1b0621ce06470b1d3d5cb8fdc24b66e820b7
im2620493 <108935058+im2620493@users.noreply.github.com>
2022-08-10 18:24:15 +0300

    test: add test case for lockRef method if address is empty

--- test/boosting/lockRefReferrerAddressIsEmpty.mjs
+++ test/boosting/lockRefReferrerAddressIsEmpty.mjs
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import { address } from '@waves/ts-lib-crypto';
 import { invokeScript, nodeInteraction as ni } from '@waves/waves-transactions';
 import { create } from '@waves/node-api-js';
 
@@ -15,16 +16,18 @@ describe('boosting: lockRefReferrerAddressIsEmpty.mjs', /** @this {MochaSuiteMod
   it(
     'should successfully lockRef is referrerAddress is empty',
     async function () {
-      const duration = 0;
+      const duration = this.maxDuration - 1;
       const referrerAddress = '';
       const signature = '';
-      const expectedPendingManagerPublicKey = null;
+      const assetAmount = this.minLockAmount;
 
       const lockRefTx = invokeScript({
-        dApp: boosting,
-        payment: [],
+        dApp: address(this.accounts.boosting, chainId),
+        payment: [
+          { assetId: this.wxAssetId, amount: assetAmount },
+        ],
         call: {
-          function: 'lockRefTx',
+          function: 'lockRef',
           args: [
             { type: 'integer', value: duration },
             { type: 'string', value: referrerAddress },
@@ -32,17 +35,98 @@ describe('boosting: lockRefReferrerAddressIsEmpty.mjs', /** @this {MochaSuiteMod
           ],
         },
         chainId,
-      }, this.accounts.manager);
+      }, this.accounts.user1);
       await api.transactions.broadcast(lockRefTx, {});
-      await ni.waitForTx(lockRefTx.id, { apiBase });
+      const { id, height, stateChanges } = await ni.waitForTx(lockRefTx.id, { apiBase });
+
+      const expectedTimestamp = (await api.blocks.fetchHeadersAt(height)).timestamp;
+      const expectedNextUserNum = 1;
+      const expectedUserNumStr = '0';
+      const expectedUserNum = 0;
+      const expectedK = 0;
+      const expectedB = 0;
+      const expectedPeriod = 0;
+      const expectedGwxAmount = 0;
+      const expectedLocksDurationSumInBlock = duration;
+      const expectedLocksCount = 1;
+      const expectedActiveUsersCount = 1;
+      const expectedActiveTotalLocked = assetAmount;
+      const expectedLockStart = height;
+      const expectedUserBoostEmissionLastIntegralKEY = 0;
+      const expectedTotalCachedGwxKEY = 499999760;
 
       expect(stateChanges.data).to.eql([{
-        key: '%s__managerPublicKey',
-        type: 'string',
-        value: anotherPublicKeyManager,
+        key: '%s__nextUserNum',
+        type: 'integer',
+        value: expectedNextUserNum,
       }, {
-        key: '%s__pendingManagerPublicKey',
-        value: expectedPendingManagerPublicKey,
+        key: `%s%s%s__mapping__user2num__${address(this.accounts.user1, chainId)}`,
+        type: 'string',
+        value: expectedUserNumStr,
+      }, {
+        key: `%s%s%s__mapping__num2user__${expectedUserNum}`,
+        type: 'string',
+        value: address(this.accounts.user1, chainId),
+      }, {
+        key: `%s%d%s__paramByUserNum__${expectedUserNum}__amount`,
+        type: 'integer',
+        value: assetAmount,
+      }, {
+        key: `%s%d%s__paramByUserNum__${expectedUserNum}__start`,
+        type: 'integer',
+        value: height,
+      }, {
+        key: `%s%d%s__paramByUserNum__${expectedUserNum}__duration`,
+        type: 'integer',
+        value: duration,
+      }, {
+        key: `%s%d%s__paramByUserNum__${expectedUserNum}__k`,
+        type: 'integer',
+        value: expectedK,
+      }, {
+        key: `%s%d%s__paramByUserNum__${expectedUserNum}__b`,
+        type: 'integer',
+        value: expectedB,
+      }, {
+        key: `%s%d%s%d__paramByPeriod__${expectedUserNum}__k__${expectedPeriod}`,
+        type: 'integer',
+        value: expectedK,
+      }, {
+        key: `%s%d%s%d__paramByPeriod__${expectedUserNum}__b__${expectedPeriod}`,
+        type: 'integer',
+        value: expectedB,
+      }, {
+        key: `%s%s__lock__${address(this.accounts.user1, chainId)}`,
+        type: 'string',
+        value: `%d%d%d%d%d%d%d%d__${expectedUserNum}__${assetAmount}__${height}__${duration}__${expectedK}__${expectedB}__${expectedTimestamp}__${expectedGwxAmount}`,
+      }, {
+        key: '%s%s__stats__locksDurationSumInBlocks',
+        type: 'integer',
+        value: expectedLocksDurationSumInBlock,
+      }, {
+        key: '%s%s__stats__locksCount',
+        type: 'integer',
+        value: expectedLocksCount,
+      }, {
+        key: '%s%s__stats__activeUsersCount',
+        type: 'integer',
+        value: expectedActiveUsersCount,
+      }, {
+        key: '%s%s__stats__activeTotalLocked',
+        type: 'integer',
+        value: expectedActiveTotalLocked,
+      }, {
+        key: `%s%s%s%s__history__lock__${address(this.accounts.user1, chainId)}__${id}`,
+        type: 'string',
+        value: `%d%d%d%d%d%d%d__${height}__${expectedTimestamp}__${assetAmount}__${expectedLockStart}__${duration}__${expectedK}__${expectedB}`,
+      }, {
+        key: `%s%d__userBoostEmissionLastInt__${expectedUserNum}`,
+        type: 'integer',
+        value: expectedUserBoostEmissionLastIntegralKEY,
+      }, {
+        key: '%s%s__gwxCached__total',
+        type: 'integer',
+        value: expectedTotalCachedGwxKEY,
       }]);
     },
   );
