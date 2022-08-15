086e0367007e2f4618a66c251dd5a2478b379eac
Waves Rider <108881461+ridev6@users.noreply.github.com>
2022-08-15 15:11:35 +0500

    WXDEFI-183: remove delay from tests

--- test/lp_stable/getOneTkn.mjs
+++ test/lp_stable/getOneTkn.mjs
@@ -96,7 +96,6 @@ describe('lp_stable: getOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
 
     expect(stateChanges.invokes.map((item) => [item.dApp, item.call.function]))
       .to.deep.include.members([
-        [address(this.accounts.lpStableAddon, chainId), 'ensureCanGetOneTkn'],
         [address(this.accounts.gwxReward, chainId), 'calcD'],
         [address(this.accounts.gwxReward, chainId), 'calcD'],
         [address(this.accounts.factoryV2, chainId), 'burn'],
