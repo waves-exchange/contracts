04a27044842bd5f46fc7ab33af8e13a493a80ec1
Waves Rider <108881461+ridev6@users.noreply.github.com>
2022-08-15 15:49:27 +0500

    WXDEFI-183: fix wxdefi-58 decimals test

--- test/lp_stable/decimals_case/getOneTkn.mjs
+++ test/lp_stable/decimals_case/getOneTkn.mjs
@@ -23,7 +23,6 @@ describe('lp_stable: getOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
 
     const exchResult = 0;
     const notUsed = 0;
-    const delay = 2;
 
     const expectedOutAmAmt = 1e10;
     const expectedOutPrAmt = 0;
@@ -48,9 +47,7 @@ describe('lp_stable: getOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
       chainId,
     }, this.accounts.user1);
     await api.transactions.broadcast(putOneTkn, {});
-    const { height } = await ni.waitForTx(putOneTkn.id, { apiBase });
-
-    await ni.waitForHeight(height + delay, { apiBase });
+    await ni.waitForTx(putOneTkn.id, { apiBase });
 
     const getOneTkn = invokeScript({
       dApp: lpStable,
@@ -101,7 +98,6 @@ describe('lp_stable: getOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
 
     expect(stateChanges.invokes.map((item) => [item.dApp, item.call.function]))
       .to.deep.include.members([
-        [address(this.accounts.lpStableAddon, chainId), 'ensureCanGetOneTkn'],
         [address(this.accounts.gwxReward, chainId), 'calcD'],
         [address(this.accounts.gwxReward, chainId), 'calcD'],
         [address(this.accounts.factoryV2, chainId), 'burn'],
