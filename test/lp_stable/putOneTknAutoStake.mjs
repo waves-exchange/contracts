64b356a90dd59a00fa95f691ef763fa89fd35f94
Waves Rider <108881461+ridev6@users.noreply.github.com>
2022-10-10 18:09:12 +0500

    WXDEFI-215: fix tests and putOneTknV2

--- test/lp_stable/putOneTknAutoStake.mjs
+++ test/lp_stable/putOneTknAutoStake.mjs
@@ -14,23 +14,40 @@ const api = create(apiBase);
 
 describe('lp_stable: putOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
   it('should successfully putOneTkn with autoStake true', async function () {
-    const amAssetPart = 1e8;
-    const prAssetPart = 1e8;
-    const outLp = 1e10;
-    const slippage = 1e3;
+    const minOutAmount = 0;
+    const slippage = 0;
     const autoStake = true;
     const usdtAmount = 1e8;
+    const usdnAmount = 1e8;
 
-    const expectedPriceLast = 1e8;
-    const expectedPriceHistory = 1e8;
+    const expectedPriceLast = 50012503;
+    const expectedPriceHistory = 50012503;
     const expectedWriteAmAmt = 1e8;
     const expectedWritePrAmt = 0;
-    const expectedEmitLpAmt = 1e10;
+    const expectedEmitLpAmt = 4996564450;
     const expectedslippageCalc = 0;
     const expectedAmDiff = 0;
     const expectedPrDiff = 0;
 
     const lpStable = address(this.accounts.lpStable, chainId);
+
+    const put = invokeScript({
+      dApp: lpStable,
+      payment: [
+        { assetId: this.usdtAssetId, amount: usdtAmount },
+        { assetId: this.usdnAssetId, amount: usdnAmount },
+      ],
+      call: {
+        function: 'put',
+        args: [
+          { type: 'integer', value: 0 },
+          { type: 'boolean', value: false },
+        ],
+      },
+      chainId,
+    }, this.accounts.user1);
+    await api.transactions.broadcast(put, {});
+    await ni.waitForTx(put.id, { apiBase });
 
     const putOneTkn = invokeScript({
       dApp: lpStable,
@@ -38,12 +55,9 @@ describe('lp_stable: putOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
         { assetId: this.usdtAssetId, amount: usdtAmount },
       ],
       call: {
-        function: 'putOneTkn',
+        function: 'putOneTknV2',
         args: [
-          { type: 'integer', value: amAssetPart },
-          { type: 'integer', value: prAssetPart },
-          { type: 'integer', value: outLp },
-          { type: 'integer', value: slippage },
+          { type: 'integer', value: minOutAmount },
           { type: 'boolean', value: autoStake },
         ],
       },
@@ -71,8 +85,6 @@ describe('lp_stable: putOneTkn.mjs', /** @this {MochaSuiteModified} */() => {
 
     expect(stateChanges.invokes.map((item) => [item.dApp, item.call.function]))
       .to.deep.include.members([
-        [address(this.accounts.gwxReward, chainId), 'calcD'],
-        [address(this.accounts.gwxReward, chainId), 'calcD'],
         [address(this.accounts.factoryV2, chainId), 'emit'],
         [address(this.accounts.staking, chainId), 'stake'],
       ]);
