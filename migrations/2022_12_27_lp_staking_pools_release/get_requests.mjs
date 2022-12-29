65277c2fbeccad2134683af90bc96cd593298736
Waves Rider <108881461+ridev6@users.noreply.github.com>
2022-12-29 18:01:05 +0500

    WXDEFI-242: reissues in migration

--- migrations/2022_12_27_lp_staking_pools_release/get_requests.mjs
+++ migrations/2022_12_27_lp_staking_pools_release/get_requests.mjs
@@ -1,32 +1,36 @@
 import { create } from '@waves/node-api-js'
 import { BigNumber } from '@waves/bignumber'
-import { data } from '@waves/waves-transactions'
+import { data, reissue, libs } from '@waves/waves-transactions'
 import { writeFile } from 'fs/promises'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
+const separator = '__'
+
 const {
   NODE_URL,
   CHAIN_ID ,
-  LP_STAKING_ADDRESS,
+  LP_STAKING_PUBLIC_KEY,
   LP_STAKING_POOLS_PUBLIC_KEY,
   USDC_ASSET_ID,
   USDT_ASSET_ID,
 } = process.env;
 const api = create(NODE_URL);
 
+const lpStakingAddress = libs.crypto.address({ publicKey: LP_STAKING_PUBLIC_KEY }, CHAIN_ID)
+
 const keyMapBaseToInternal = (baseAsset) =>
   `%s%s%s__mappings__baseAsset2internalId__${baseAsset}`;
 const { value: usdtInternalAssetId } = await api.addresses.fetchDataKey(
-  LP_STAKING_ADDRESS,
+  lpStakingAddress,
   keyMapBaseToInternal(USDT_ASSET_ID)
 );
 const { value: usdcInternalAssetId } = await api.addresses.fetchDataKey(
-  LP_STAKING_ADDRESS,
+  lpStakingAddress,
   keyMapBaseToInternal(USDC_ASSET_ID)
 );
-const operationsData = await api.addresses.data(LP_STAKING_ADDRESS, {
+const operationsData = await api.addresses.data(lpStakingAddress, {
   matches: encodeURIComponent(`%s%d%s%s__G__(${usdtInternalAssetId}|${usdcInternalAssetId})__.+`),
 });
 const internalToBaseAssetId = {
@@ -39,20 +43,30 @@ const requestsInfo = {
 }
 for (const { key, value } of operationsData) {
   const statusPending = 'PENDING'
-  const internalBaseAssetId = key.split('__')[2]
+  const internalBaseAssetId = key.split(separator)[2]
   const baseAssetId = internalToBaseAssetId[internalBaseAssetId]
-  const userAddress = key.split('__')[3]
-  const status = value.split('__')[1]
-  const inAssetAmount = new BigNumber(value.split('__')[2])
+  const userAddress = key.split(separator)[3]
+  const status = value.split(separator)[1]
+  const inAssetAmount = new BigNumber(value.split(separator)[2])
   if (status != statusPending) continue
   requestsInfo[baseAssetId]['users'][userAddress] = (requestsInfo[baseAssetId]['users'][userAddress] || new BigNumber(0)).add(inAssetAmount)
   requestsInfo[baseAssetId]['totalAmount'] = requestsInfo[baseAssetId]['totalAmount'].add(inAssetAmount)
 }
 
-const keyShareAssetAmountToConvert = (baseAssetId) => ['%s%s', baseAssetId, 'shareAssetAmountToConvert'].join('__')
-const keyUserShareAssetAmountToConvert = (baseAssetId, userAddress) => ['%s%s%s', baseAssetId, userAddress, 'shareAssetAmountToConvert'].join('__')
+const keyShareAssetAmountToConvert = (baseAssetId) => ['%s%s', baseAssetId, 'shareAssetAmountToConvert'].join(separator)
+const keyUserShareAssetAmountToConvert = (baseAssetId, userAddress) => ['%s%s%s', baseAssetId, userAddress, 'shareAssetAmountToConvert'].join(separator)
+const keyAssetConfig = (baseAsset) => ['%s%s%s', 'config', 'asset', baseAsset].join(separator)
 const actions = []
+const reissues = []
 for (const baseAsset in requestsInfo) {
+  const { value: baseAssetConfig } = await api.addresses.fetchDataKey(lpStakingAddress, keyAssetConfig(baseAsset))
+  const shareAssetId = baseAssetConfig.split(separator)[1]
+  reissues.push(reissue({
+    assetId: shareAssetId,
+    quantity: requestsInfo[baseAsset]['totalAmount'].toString(),
+    reissuable: true,
+    senderPublicKey: LP_STAKING_PUBLIC_KEY,
+  }))
   actions.push({
     key: keyShareAssetAmountToConvert(baseAsset),
     type: 'integer',
@@ -79,4 +93,5 @@ const dataTxs = actionsChunks.map((changes) => data({
   additionalFee: 4e5,
 }));
 
+await writeFile('reissues.json', JSON.stringify(reissues, null, 2))
 await writeFile('get_requests.json', JSON.stringify(dataTxs, null, 2))
