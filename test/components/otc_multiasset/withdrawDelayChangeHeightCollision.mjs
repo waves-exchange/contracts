384cf2c63319865960c0cc91015792fa6e28b836
bra1nsurfer <brainsurfer@tr0n.tk>
2023-01-31 14:02:23 +0300

    Renamed test

--- test/components/otc_multiasset/withdrawDelayChangeHeightCollision.mjs
+++ test/components/otc_multiasset/withdrawDelayChangeHeightCollision.mjs
@@ -17,7 +17,7 @@ const addedDelay = 2;
 const api = create(apiBase);
 
 describe('otc_multiasset: withdrawDelayChangeHeightCollision.mjs', /** @this {MochaSuiteModified} */() => {
-  it('should be same height error', async function () {
+  it('should reject with same height error', async function () {
     const amountAssetA = this.minAmountDeposit + 1;
     const amountAssetB = this.minAmountWithdraw;
 
