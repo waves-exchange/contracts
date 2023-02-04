0d58a6cabf6f8351cb35104d3d99dd4e5cbec214
cringe013 <98390412+cringe013@users.noreply.github.com>
2023-02-04 17:26:11 +0400

    fix

--- deployer/pkg/cli_contract/contract.go
+++ deployer/pkg/cli_contract/contract.go
@@ -2,6 +2,7 @@ package cli_contract
 
 import (
 	"context"
+	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/wavesplatform/gowaves/pkg/client"
 	"github.com/wavesplatform/gowaves/pkg/crypto"
@@ -12,32 +13,66 @@ import (
 type Contract struct {
 	logger      zerolog.Logger
 	client      *client.Client
-	prv         crypto.SecretKey
+	basePrv     crypto.SecretKey
+	signerPrv   crypto.SecretKey
+	networkByte proto.Scheme
 	tag         string
 	filename    string
 	compact     bool
 	data        []proto.DataEntry
-	constructor proto.InvokeScriptWithProofs
+	constructor []proto.InvokeScriptWithProofs
 }
 
 func New(
 	client *client.Client,
-	prv crypto.SecretKey,
+	basePrv crypto.SecretKey,
+	signerPrv crypto.SecretKey,
 	tag string,
 	filename string,
+	compact bool,
 	data []proto.DataEntry,
+	constructor []proto.InvokeScriptWithProofs,
 ) Contract {
 	return Contract{
 		logger: zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).Level(zerolog.DebugLevel).With().Timestamp().
 			Caller().Logger(),
-		client:   client,
-		prv:      prv,
-		tag:      tag,
-		filename: filename,
-		data:     data,
+		client:      client,
+		basePrv:     basePrv,
+		signerPrv:   signerPrv,
+		tag:         tag,
+		filename:    filename,
+		compact:     compact,
+		data:        data,
+		constructor: constructor,
 	}
 }
 
 func (c Contract) Deploy(ctx context.Context) error {
-	dataTx := proto.newunsigneddata
+	err := c.setData(ctx)
+	if err != nil {
+		return fmt.Errorf("c.setData: %w", err)
+	}
+
+	err = c.setScript(ctx)
+	if err != nil {
+		return fmt.Errorf("c.setScript: %w", err)
+	}
+
+	err = c.callConstructor(ctx)
+	if err != nil {
+		return fmt.Errorf("c.callConstructor: %w", err)
+	}
+	return nil
+}
+
+func (c Contract) setData(ctx context.Context) error {
+
+}
+
+func (c Contract) callConstructor(ctx context.Context) error {
+
+}
+
+func (c Contract) setScript(ctx context.Context) error {
+
 }
--- deployer/pkg/tools/tools.go
+++ deployer/pkg/tools/tools.go
@@ -1,8 +1,12 @@
 package tools
 
 import (
+	"context"
 	"encoding/binary"
+	"fmt"
+	"github.com/wavesplatform/gowaves/pkg/client"
 	"github.com/wavesplatform/gowaves/pkg/crypto"
+	"github.com/wavesplatform/gowaves/pkg/proto"
 	"math"
 	"time"
 )
@@ -34,4 +38,58 @@ func CalcSetScriptFee(script []byte) uint64 {
 
 func Timestamp() uint64 {
 	return uint64(time.Now().UnixMilli())
+}
+
+func SignBroadcastWait(
+	ctx context.Context,
+	networkByte proto.Scheme,
+	client *client.Client,
+	tx proto.Transaction,
+	prv crypto.SecretKey,
+) error {
+	_, err := tx.Validate(networkByte)
+	if err != nil {
+		return fmt.Errorf("tx.Validate: %w", err)
+	}
+
+	err = tx.Sign(networkByte, prv)
+	if err != nil {
+		return fmt.Errorf("tx.Sign: %w", err)
+	}
+
+	txHashBytes, err := tx.GetID(networkByte)
+	if err != nil {
+		return fmt.Errorf("tx.GetID: %w", err)
+	}
+
+	txHash, err := crypto.NewDigestFromBytes(txHashBytes)
+	if err != nil {
+		return fmt.Errorf("crypto.NewDigestFromBytes: %w", err)
+	}
+
+	_, e := client.Transactions.Broadcast(ctx, tx)
+	if e != nil {
+		return fmt.Errorf("client.Transactions.Broadcast: %w", e)
+	}
+
+	e = waitMined(ctx, client, txHash)
+	if e != nil {
+		return fmt.Errorf("waitMined: %w", e)
+	}
+	return nil
+}
+
+func waitMined(ctx context.Context, client *client.Client, txHash crypto.Digest) error {
+	try := 0
+	for {
+		try += 1
+		_, _, err := client.Transactions.Info(ctx, txHash)
+		if err == nil {
+			return nil
+		}
+		if try >= 100 {
+			return fmt.Errorf("client.Transactions.Info: %w", err)
+		}
+		time.Sleep(10 * time.Second)
+	}
 }
