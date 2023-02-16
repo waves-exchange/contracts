1a2ba0172fd1dabb1c20abe2782b41b4ac2ac68b
cringe013 <98390412+cringe013@users.noreply.github.com>
2023-02-16 18:45:56 +0400

    fix

--- deployer/pkg/branch/branch.go
+++ deployer/pkg/branch/branch.go
@@ -7,7 +7,6 @@ import (
 	"github.com/waves-exchange/contracts/deployer/pkg/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Branch struct {
@@ -19,30 +18,10 @@ type Model struct {
 	coll *mongo.Collection
 }
 
-func NewModel(coll *mongo.Collection) (Model, error) {
-	m := Model{
+func NewModel(coll *mongo.Collection) Model {
+	return Model{
 		coll: coll,
 	}
-	err := m.createIndex()
-	if err != nil {
-		return Model{}, fmt.Errorf("m.createIndex: %w", err)
-	}
-
-	return m, nil
-}
-
-func (m Model) createIndex() error {
-	_, err := m.coll.Indexes().CreateOne(context.Background(), mongo.IndexModel{
-		Keys: bson.M{
-			"network": 1,
-		},
-		Options: options.Index().SetUnique(true),
-	})
-	if err != nil {
-		return fmt.Errorf("m.coll.Indexes().CreateOne: %w", err)
-	}
-
-	return nil
 }
 
 func (m Model) GetTestnetBranch(ctx context.Context) (string, error) {
