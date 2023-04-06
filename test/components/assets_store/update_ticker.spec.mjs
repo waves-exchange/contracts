ef9abe92ecac7cecdf8ca42e392fe468bc9f64d8
Waves Rider <108881461+ridev6@users.noreply.github.com>
2023-04-06 18:23:17 +0500

    wxdefi-345: + test

--- test/components/assets_store/update_ticker.spec.mjs
+++ test/components/assets_store/update_ticker.spec.mjs
@@ -17,7 +17,8 @@ const keyTickerToAssetId = (ticker) => `%s%s__ticker2assetId__${ticker}`;
 /** @typedef {Mocha.Suite & {accounts: Object.<string, number>}} MochaSuiteModified */
 describe('Assets store - update ticker', /** @this {MochaSuiteModified} */() => {
   /** @type {string} */
-  let assetId;
+  let assetId1;
+  let assetId2;
   const ticker1 = 'ticker1';
   const ticker2 = 'ticker2';
   before(async function () {
@@ -35,26 +36,41 @@ describe('Assets store - update ticker', /** @this {MochaSuiteModified} */() => 
     }, this.accounts.store);
     await broadcastAndWait(constructorInvokeTx);
 
-    const assetIssueTx = issue({
-      name: 'base asset', description: '', quantity: 1e8, decimals: 8, chainId,
-    }, baseSeed);
-    await broadcastAndWait(assetIssueTx);
-    assetId = assetIssueTx.id;
+    ({ id: assetId1 } = await broadcastAndWait(issue({
+      name: 'asset 1', description: '', quantity: 1e8, decimals: 8, chainId,
+    }, baseSeed)));
 
-    const createOrUpdateInvokeTx = invokeScript({
+    ({ id: assetId2 } = await broadcastAndWait(issue({
+      name: 'asset 2', description: '', quantity: 1e8, decimals: 8, chainId,
+    }, baseSeed)));
+
+    await broadcastAndWait(invokeScript({
       dApp: address(this.accounts.store, chainId),
       call: {
         function: 'createOrUpdate',
         args: [
-          { type: 'string', value: assetId }, // assetId
+          { type: 'string', value: assetId1 }, // assetId
           { type: 'string', value: '' }, // logo
           { type: 'boolean', value: false }, // verified
         ],
       },
       fee: 9e5,
       chainId,
-    }, this.accounts.store);
-    await broadcastAndWait(createOrUpdateInvokeTx);
+    }, this.accounts.store));
+
+    await broadcastAndWait(invokeScript({
+      dApp: address(this.accounts.store, chainId),
+      call: {
+        function: 'createOrUpdate',
+        args: [
+          { type: 'string', value: assetId2 },
+          { type: 'string', value: '' },
+          { type: 'boolean', value: false },
+        ],
+      },
+      fee: 9e5,
+      chainId,
+    }, this.accounts.store));
   });
 
   it('Should set ticker', async function () {
@@ -63,7 +79,7 @@ describe('Assets store - update ticker', /** @this {MochaSuiteModified} */() => 
       call: {
         function: 'updateTicker',
         args: [
-          { type: 'string', value: assetId },
+          { type: 'string', value: assetId1 },
           { type: 'string', value: ticker1 },
         ],
       },
@@ -74,14 +90,14 @@ describe('Assets store - update ticker', /** @this {MochaSuiteModified} */() => 
 
     expect(stateChanges.data).to.eql([
       {
-        key: keyAssetIdToTicker(assetId),
+        key: keyAssetIdToTicker(assetId1),
         type: 'string',
         value: ticker1,
       },
       {
         key: keyTickerToAssetId(ticker1),
         type: 'string',
-        value: assetId,
+        value: assetId1,
       },
     ]);
   });
@@ -92,7 +108,7 @@ describe('Assets store - update ticker', /** @this {MochaSuiteModified} */() => 
       call: {
         function: 'updateTicker',
         args: [
-          { type: 'string', value: assetId },
+          { type: 'string', value: assetId1 },
           { type: 'string', value: ticker2 },
         ],
       },
@@ -104,14 +120,44 @@ describe('Assets store - update ticker', /** @this {MochaSuiteModified} */() => 
     expect(stateChanges.data).to.eql([
       { key: keyTickerToAssetId(ticker1), value: null },
       {
-        key: keyAssetIdToTicker(assetId),
+        key: keyAssetIdToTicker(assetId1),
         type: 'string',
         value: ticker2,
       },
       {
         key: keyTickerToAssetId(ticker2),
         type: 'string',
-        value: assetId,
+        value: assetId1,
+      },
+    ]);
+  });
+
+  it('Should update assetId and remove old one', async function () {
+    const addLabelInvokeTx = invokeScript({
+      dApp: address(this.accounts.store, chainId),
+      call: {
+        function: 'updateTicker',
+        args: [
+          { type: 'string', value: assetId2 },
+          { type: 'string', value: ticker2 },
+        ],
+      },
+      fee: 9e5,
+      chainId,
+    }, this.accounts.store);
+    const { stateChanges } = await broadcastAndWait(addLabelInvokeTx);
+
+    expect(stateChanges.data).to.eql([
+      { key: keyAssetIdToTicker(assetId1), value: null },
+      {
+        key: keyAssetIdToTicker(assetId2),
+        type: 'string',
+        value: ticker2,
+      },
+      {
+        key: keyTickerToAssetId(ticker2),
+        type: 'string',
+        value: assetId2,
       },
     ]);
   });
