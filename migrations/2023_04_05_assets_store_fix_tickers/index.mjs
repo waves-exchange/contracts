7ebc9a2591ee7633b29babfeeb05ed1d4bec90fa
Waves Rider <108881461+ridev6@users.noreply.github.com>
2023-04-06 13:04:19 +0500

    wxdefi-345: txs generator

--- migrations/2023_04_05_assets_store_fix_tickers/index.mjs
+++ migrations/2023_04_05_assets_store_fix_tickers/index.mjs
@@ -1,79 +1,114 @@
-import { create } from '@waves/node-api-js';
-import { data } from '@waves/waves-transactions';
-import { join } from 'path';
-import { readdir, unlink, writeFile } from 'fs/promises';
-import { address } from '@waves/ts-lib-crypto';
-import config from './config.mjs';
+import { create } from '@waves/node-api-js'
+import { data } from '@waves/waves-transactions'
+import { join } from 'path'
+import { readdir, unlink, writeFile } from 'fs/promises'
+import { address } from '@waves/ts-lib-crypto'
+import config from './config.mjs'
 
-const network = 'mainnet';
-const {
-  nodeUrl,
-  chainId,
-  assetsStorePublicKey,
-} = config[network];
+const network = 'mainnet'
+const { nodeUrl, chainId, assetsStorePublicKey } = config[network]
 
-const assetsStoreAddress = address({ publicKey: assetsStorePublicKey }, chainId);
+const separator = '__'
 
-const nodeApi = create(nodeUrl);
+const assetsStoreAddress = address({ publicKey: assetsStorePublicKey }, chainId)
 
-const keyAssetIdToTicker = (assetId) => `%s%s__assetId2ticker__${assetId}`;
-const entryAssetIdToTicker = (assetId, ticker = '') => ({ key: keyAssetIdToTicker(assetId), type: 'string', value: ticker });
+const nodeApi = create(nodeUrl)
 
-const keyTickerToAssetId = (ticker) => `%s%s__ticker2assetId__${ticker}`;
-const entryTickerToAssetId = (ticker, assetId) => ({ key: keyTickerToAssetId(ticker), type: 'string', value: assetId });
+const keyAssetIdToTicker = (assetId) => `%s%s__assetId2ticker__${assetId}`
+const keyTickerToAssetId = (ticker) => `%s%s__ticker2assetId__${ticker}`
 
 const fetchCurrentState = async () => {
-  const assetIdToTicker = await nodeApi.addresses.data(assetsStoreAddress, {
-    matches: encodeURIComponent(keyAssetIdToTicker('.+')),
-  }, {});
+  const [assetIdToTickerData, tickerToAssetIdData] = await Promise.all([
+    nodeApi.addresses.data(
+      assetsStoreAddress,
+      {
+        matches: encodeURIComponent(keyAssetIdToTicker('.+'))
+      },
+      {}
+    ),
+    nodeApi.addresses.data(
+      assetsStoreAddress,
+      {
+        matches: encodeURIComponent(keyTickerToAssetId('.+'))
+      },
+      {}
+    )
+  ])
 
-  const tickerToAssetId = await nodeApi.addresses.data(assetsStoreAddress, {
-    matches: encodeURIComponent(keyTickerToAssetId('.+')),
-  }, {});
-
-  return { assetIdToTicker, tickerToAssetId };
-};
+  const targetIndex = 2
+  const formatData = (arr) =>
+    Array.prototype.reduce.call(
+      arr,
+      (acc, { key, value }) => ({
+        ...acc,
+        [key.split(separator)[targetIndex]]: value
+      }),
+      {}
+    )
+  return {
+    assetIdToTicker: formatData(assetIdToTickerData),
+    tickerToAssetId: formatData(tickerToAssetIdData)
+  }
+}
 
 const getTxs = async () => {
-  const currentState = await fetchCurrentState();
-  console.log(currentState);
+  const { assetIdToTicker, tickerToAssetId } = await fetchCurrentState()
+  for (let assetId in assetIdToTicker) {
+    const ticker = assetIdToTicker[assetId]
+    if (tickerToAssetId[ticker] == assetId) {
+      delete tickerToAssetId[ticker]
+    }
+  }
 
-  const actions = [];
+  const actions = Object.keys(tickerToAssetId).map((ticker) => ({
+    key: keyTickerToAssetId(ticker)
+  }))
 
-  const chunkSize = 100;
-  const actionsChunks = Object.values(actions.reduce((acc, cur, idx) => {
-    const chunkIndex = Math.floor(idx / chunkSize);
-    return {
-      ...acc,
-      [chunkIndex]: [...(acc[chunkIndex] || []), cur],
-    };
-  }, {}));
+  const chunkSize = 100
+  const actionsChunks = Object.values(
+    actions.reduce((acc, cur, idx) => {
+      const chunkIndex = Math.floor(idx / chunkSize)
+      return {
+        ...acc,
+        [chunkIndex]: [...(acc[chunkIndex] || []), cur]
+      }
+    }, {})
+  )
 
-  return actionsChunks.map((changes) => data({
-    data: changes,
-    chainId,
-    senderPublicKey: assetsStorePublicKey,
-    additionalFee: 4e5,
-  }));
-};
+  return actionsChunks.map((changes) =>
+    data({
+      data: changes,
+      chainId,
+      senderPublicKey: assetsStorePublicKey,
+      additionalFee: 4e5
+    })
+  )
+}
 
 try {
-  const txsPath = 'txs';
+  const txsPath = 'txs'
+
   // clear txs dir
-  const exclude = ['.gitignore'];
-  const files = await readdir(txsPath);
-  await Promise.all(files.map(async (name) => {
-    if (exclude.includes(name)) return name;
-    return unlink(join(txsPath, name));
-  }));
+  const exclude = ['.gitignore']
+  const files = await readdir(txsPath)
+  await Promise.all(
+    files.map(async (name) => {
+      if (exclude.includes(name)) return name
+      return unlink(join(txsPath, name))
+    })
+  )
 
-  // create transactions
-  const txs = (await getTxs()).map((tx) => [tx, 'fill_state']);
+  const fileName = 'delete_broken_tickers'
+  const txs = (await getTxs()).map((tx) => [tx, fileName])
 
-  // save txs
-  await Promise.all(txs.map(async ([tx, name], idx) => {
-    await writeFile(join(txsPath, `${idx}${name ? '_' : ''}${name || ''}.json`), JSON.stringify(tx, null, 2));
-  }));
+  await Promise.all(
+    txs.map(async ([tx, name], idx) => {
+      await writeFile(
+        join(txsPath, `${idx}${name ? '_' : ''}${name || ''}.json`),
+        JSON.stringify(tx, null, 2)
+      )
+    })
+  )
 } catch (error) {
-  console.error(error);
+  console.error(error)
 }
