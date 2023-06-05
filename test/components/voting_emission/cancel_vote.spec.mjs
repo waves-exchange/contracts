2e48258d4310c50fe9af4b74276c9e4de9e47e6c
bra1nsurfer <brainsurfer@tr0n.tk>
2023-06-05 10:02:39 +0300

    votinge_emission tests fixes

--- test/components/voting_emission/cancel_vote.spec.mjs
+++ test/components/voting_emission/cancel_vote.spec.mjs
@@ -24,6 +24,7 @@ describe(`${process.pid}: voting_emission: cancel vote`, () => {
       votingEmissionCandidateContract: this.accounts.votingEmissionCandidate.addr,
       boostingContract: this.accounts.boosting.addr,
       stakingContract: this.accounts.staking.addr,
+      votingEmissionRate: this.accounts.votingEmissionRate.addr,
       epochLength,
     });
     await votingEmission.create({
