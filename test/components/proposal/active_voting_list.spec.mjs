77f91fd4720590a1cc88c1c5af43a61a9c356a40
Waves Rider <108881461+ridev6@users.noreply.github.com>
2023-07-25 18:38:54 +0500

    updateActiveVotingListActions + test

--- test/components/proposal/active_voting_list.spec.mjs
+++ test/components/proposal/active_voting_list.spec.mjs
@@ -1,17 +1,21 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { data } from '@waves/waves-transactions';
-import { api, broadcastAndWait, chainId } from '../../utils/api.mjs';
+import {
+  api, broadcastAndWait, chainId, waitForHeight,
+} from '../../utils/api.mjs';
 import { proposal } from './contract/proposal.mjs';
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
-describe(`${process.pid}: proposal: locked gwx amount`, () => {
-  it('get locked gwx amount should work', async function () {
+describe(`${process.pid}: proposal: active voting list`, () => {
+  let startHeight;
+  it('list should be updated after vote creation', async function () {
+    const keyActiveVotingList = '%s__activeVotingList';
     const duration = 60;
     const quorumNumber = 1e8;
-    await Promise.all([
+    [{ height: startHeight }] = await Promise.all([
       proposal.startNewVote({
         dApp: this.accounts.proposal.address,
         caller: this.accounts.proposal.seed,
@@ -25,7 +29,7 @@ describe(`${process.pid}: proposal: locked gwx amount`, () => {
         caller: this.accounts.proposal.seed,
         name: 'test2',
         description: 'test2',
-        duration,
+        duration: 1,
         quorumNumber,
       }),
       proposal.startNewVote({
@@ -38,6 +42,16 @@ describe(`${process.pid}: proposal: locked gwx amount`, () => {
       }),
     ]);
 
+    expect(
+      await api.addresses.fetchDataKey(
+        this.accounts.proposal.address,
+        keyActiveVotingList,
+      ).then(({ value }) => value),
+    ).to.equal('2__1__0');
+  });
+
+  it('list should be updated when voting is over', async function () {
+    await waitForHeight(startHeight + 2);
     const vote1 = 1e8;
     await broadcastAndWait(data({
       data: [
@@ -49,38 +63,17 @@ describe(`${process.pid}: proposal: locked gwx amount`, () => {
       chainId,
     }, this.accounts.gwxReward.seed));
 
-    await proposal.voteFor({
+    const { stateChanges } = await proposal.voteFor({
       dApp: this.accounts.proposal.address,
       caller: this.accounts.user0.seed,
       proposalIndex: 0,
       choice: true,
     });
 
-    const vote2 = 2e8;
-    await broadcastAndWait(data({
-      data: [
-        {
-          key: `%s%s__gwxAmountTotal__${this.accounts.user0.address}`,
-          value: vote2,
-        },
-      ],
-      chainId,
-    }, this.accounts.gwxReward.seed));
-
-    await proposal.voteFor({
-      dApp: this.accounts.proposal.address,
-      caller: this.accounts.user0.seed,
-      proposalIndex: 2,
-      choice: true,
+    expect(stateChanges.data).to.deep.include({
+      key: '%s__activeVotingList',
+      type: 'string',
+      value: '2__0',
     });
-
-    const expr = `getLockedGwxAmount("${this.accounts.user0.address}")`;
-    const response = await api.utils.fetchEvaluate(
-      this.accounts.proposal.address,
-      expr,
-    );
-    const result = response.result.value._2.value;
-
-    expect(result).to.equal(Math.max(vote1, vote2));
   });
 });
