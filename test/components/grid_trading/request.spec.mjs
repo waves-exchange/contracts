011c74949b79c85a6aab5de8be7bc2fc1d17da79
Waves Rider <108881461+ridev6@users.noreply.github.com>
2023-10-10 16:00:27 +0500

    successfull request

--- test/components/grid_trading/request.spec.mjs
+++ test/components/grid_trading/request.spec.mjs
@@ -1,8 +1,10 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { invokeScript } from '@waves/waves-transactions';
-import { base64Encode } from '@waves/ts-lib-crypto';
-import { chainId, broadcastAndWait } from '../../utils/api.mjs';
+import { data, invokeScript, transfer } from '@waves/waves-transactions';
+import {
+  base58Decode, base58Encode, base64Encode, sha256,
+} from '@waves/ts-lib-crypto';
+import { chainId, broadcastAndWait, baseSeed } from '../../utils/api.mjs';
 import { setup } from './_setup.mjs';
 
 chai.use(chaiAsPromised);
@@ -20,20 +22,132 @@ describe(`[${process.pid}] grid_trading: request`, () => {
     } = await setup());
   });
 
+  it('1 payment is required', async () => {
+    expect(broadcastAndWait(invokeScript({
+      dApp: accounts.factory.address,
+      call: {
+        function: 'request',
+        args: [
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId1))}` },
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId2))}` },
+        ],
+      },
+      payment: [],
+      chainId,
+    }, accounts.user1.seed))).to.be.rejectedWith('1 payment is required');
+  });
+
+  it('invalid asset', async () => {
+    await broadcastAndWait(transfer({
+      recipient: accounts.user1.address,
+      assetId: assetId1,
+      amount: 1e8,
+    }, baseSeed));
+    expect(broadcastAndWait(invokeScript({
+      dApp: accounts.factory.address,
+      call: {
+        function: 'request',
+        args: [
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId1))}` },
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId2))}` },
+        ],
+      },
+      payment: [
+        { assetId: assetId1, amount: rewardAmount },
+      ],
+      chainId,
+    }, accounts.user1.seed))).to.be.rejectedWith('invalid asset');
+  });
+
+  it('invalid amount', async () => {
+    expect(broadcastAndWait(invokeScript({
+      dApp: accounts.factory.address,
+      call: {
+        function: 'request',
+        args: [
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId1))}` },
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId2))}` },
+        ],
+      },
+      payment: [
+        { assetId: null, amount: rewardAmount - 1 },
+      ],
+      chainId,
+    }, accounts.user1.seed))).to.be.rejectedWith('pair is not allowed');
+  });
+
   it('pair is not allowed', async () => {
     expect(broadcastAndWait(invokeScript({
       dApp: accounts.factory.address,
       call: {
         function: 'request',
         args: [
-          { type: 'binary', value: base64Encode(base58Decode(assetId1)) },
-          { type: 'binary', value: base64Encode(base58Decode(assetId2)) },
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId1))}` },
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId2))}` },
         ],
       },
       payment: [
         { assetId: null, amount: rewardAmount },
       ],
       chainId,
-    }, accounts.factory.seed))).to.be.rejectedWith('pair is not allowed');
+    }, accounts.user1.seed))).to.be.rejectedWith('pair is not allowed');
+  });
+
+  it('successfully create request', async () => {
+    await broadcastAndWait(data({
+      data: [
+        { key: `%s%s%s__${assetId1}__${assetId2}__pairAllowed`, type: 'boolean', value: true },
+      ],
+      chainId,
+    }, accounts.factory.seed)).catch(({ message }) => { throw new Error(message); });
+
+    const { stateChanges } = await broadcastAndWait(invokeScript({
+      dApp: accounts.factory.address,
+      call: {
+        function: 'request',
+        args: [
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId1))}` },
+          { type: 'binary', value: `base64:${base64Encode(base58Decode(assetId2))}` },
+        ],
+      },
+      payment: [
+        { assetId: null, amount: rewardAmount },
+      ],
+      chainId,
+    }, accounts.user1.seed)).catch(({ message }) => { throw new Error(message); });
+
+    // let accountId = sha256(i.caller.bytes + amountAssetId + priceAssetId).toBase58String()
+    const accountId = base58Encode(sha256([
+      ...base58Decode(accounts.user1.address),
+      ...base58Decode(assetId1),
+      ...base58Decode(assetId2),
+    ]));
+
+    const accountStatusEmpty = 0;
+
+    const expected = [
+      {
+        key: `%s%s__${accountId}__status`,
+        type: 'integer',
+        value: accountStatusEmpty,
+      },
+      {
+        key: `%s%s__${accountId}__ownerPublicKey`,
+        type: 'binary',
+        value: `base64:${base64Encode(base58Decode(accounts.user1.publicKey))}`,
+      },
+      {
+        key: `%s%s__${accountId}__amountAssetId`,
+        type: 'binary',
+        value: `base64:${base64Encode(base58Decode(assetId1))}`,
+      },
+      {
+        key: `%s%s__${accountId}__priceAssetId`,
+        type: 'binary',
+        value: `base64:${base64Encode(base58Decode(assetId2))}`,
+      },
+    ];
+
+    expect(stateChanges.data).to.deep.equal(expected);
   });
 });
