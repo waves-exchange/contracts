c4b9d60f76360170acbb87218c15623972f164a4
bra1nsurfer <brainsurfer@tr0n.tk>
2024-02-14 23:34:45 +0300

    rename functions test boilerplate

---
+++ test/components/l2mp_leasing/_hooks.mjs
@@ -0,0 +1,92 @@
+import { address, randomSeed, publicKey } from '@waves/ts-lib-crypto';
+import {
+  data,
+  massTransfer,
+  issue,
+} from '@waves/waves-transactions';
+import { format } from 'path';
+import { setScriptFromFile } from '../../utils/utils.mjs';
+
+import {
+  broadcastAndWait, chainId, baseSeed,
+} from '../../utils/api.mjs';
+
+const seedWordsCount = 5;
+const ridePath = '../ride';
+const l2mpLeasingPath = format({ dir: ridePath, base: 'l2mp_leasing.ride' });
+
+export const mochaHooks = {
+  async beforeAll() {
+    const names = [
+      'l2mpLeasing',
+      'admin1',
+      'user1',
+      'user2',
+      'node1',
+    ];
+    this.accounts = Object.fromEntries(names.map((item) => {
+      const itemSeed = randomSeed(seedWordsCount);
+      return [
+        item,
+        { seed: itemSeed, addr: address(itemSeed, chainId), publicKey: publicKey(itemSeed) },
+      ];
+    }));
+    const amount = 100e8;
+    const massTransferTx = massTransfer({
+      transfers: Object.values(this.accounts).map((item) => ({ recipient: item.addr, amount })),
+      chainId,
+    }, baseSeed);
+    await broadcastAndWait(massTransferTx);
+
+    const issueAsset = issue({
+      quantity: 1000000_0000_0000,
+      decimals: 8,
+      name: 'L2MP',
+      description: 'Mining Power Token for WAVES EVM L2 bootstrapping.',
+      chainId,
+    }, this.accounts.l2mpLeasing.seed);
+
+    this.l2mpAssetId = issueAsset.id;
+
+    await broadcastAndWait(issueAsset);
+
+    const massTransferAssetTx = massTransfer({
+      transfers: Object.values(this.accounts).map((item) => ({ recipient: item.addr, amount })),
+      chainId,
+      assetId: issueAsset.id,
+    }, this.accounts.l2mpLeasing.seed);
+    await broadcastAndWait(massTransferAssetTx);
+
+    await setScriptFromFile(l2mpLeasingPath, this.accounts.l2mpStaking.seed);
+
+    // const adminsListString = [
+    //   this.accounts.admin1.addr,
+    //   this.accounts.admin2.addr,
+    //   this.accounts.admin3.addr,
+    // ].join('__');
+
+    const dataTx = data({
+      additionalFee: 4e5,
+      data: [
+        // {
+        //   key: '%s__adminAddressList',
+        //   type: 'string',
+        //   value: adminsListString,
+        // },
+        {
+          key: '%s__assetId',
+          type: 'string',
+          value: this.l2mpAssetId,
+        },
+        {
+          key: '%s__emissionPeriodInBlocks',
+          type: 'integer',
+          value: 1,
+        },
+      ],
+      chainId,
+    }, this.accounts.l2mpLeasing.seed);
+
+    await broadcastAndWait(dataTx);
+  },
+};
---
+++ test/components/l2mp_leasing/getAssetsInfo.spec.mjs
@@ -0,0 +1,143 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { invokeScript } from '@waves/waves-transactions';
+import {
+  api, chainId, broadcastAndWait,
+} from '../../utils/api.mjs';
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe('l2mp_staking: get staked info', /** @this {MochaSuiteModified} */() => {
+  const stakeAmount1 = 12345678;
+  const stakeAmount2 = 2e8;
+  const lockedAmount1 = 654321;
+
+  before(
+    async function () {
+      const stakeTx1 = invokeScript({
+        dApp: this.accounts.l2mpLeasing.addr,
+        call: {
+          function: 'stakeAndSetStakingNode',
+          args: [{
+            type: 'string',
+            value: this.accounts.node1.addr,
+          }],
+        },
+        payment: [
+          { assetId: this.l2mpAssetId, amount: stakeAmount1 },
+        ],
+        additionalFee: 4e5,
+        chainId,
+      }, this.accounts.user1.seed);
+
+      const stakeTx2 = invokeScript({
+        dApp: this.accounts.l2mpLeasing.addr,
+        call: {
+          function: 'stake',
+        },
+        payment: [
+          { assetId: this.l2mpAssetId, amount: stakeAmount2 },
+        ],
+        additionalFee: 4e5,
+        chainId,
+      }, this.accounts.user2.seed);
+
+      const airdropTx = invokeScript({
+        dApp: this.accounts.l2mpLeasing.addr,
+        call: {
+          function: 'airdrop',
+          args: [{
+            type: 'list',
+            value: [
+              {
+                type: 'string',
+                value: this.accounts.user1.addr,
+              },
+            ],
+          },
+          {
+            type: 'list',
+            value: [
+              {
+                type: 'integer',
+                value: lockedAmount1,
+              },
+            ],
+          }],
+        },
+        payment: [
+          { assetId: this.l2mpAssetId, amount: lockedAmount1 },
+        ],
+        additionalFee: 4e5,
+        chainId,
+      }, this.accounts.user1.seed);
+
+      await broadcastAndWait(stakeTx1);
+      await broadcastAndWait(stakeTx2);
+      await broadcastAndWait(airdropTx);
+    },
+  );
+
+  it(
+    'getUserAssetsREADONLY should return correct values',
+    async function () {
+      const expr = `getUserAssetsREADONLY("${this.accounts.user1.addr}")`;
+      const response = await api.utils.fetchEvaluate(
+        this.accounts.l2mpStaking.addr,
+        expr,
+      );
+      const checkData = response.result.value._2;
+
+      expect(checkData).to.eql({
+        type: 'Tuple',
+        value: {
+          _1: { type: 'Int', value: stakeAmount1 },
+          _2: { type: 'Int', value: stakeAmount1 },
+          _3: { type: 'BigInt', value: `${10e17}` },
+          _4: { type: 'Int', value: stakeAmount1 },
+          _5: { type: 'Int', value: 0 },
+          _6: { type: 'Int', value: lockedAmount1 },
+          _7: { type: 'Int', value: lockedAmount1 },
+          _8: {
+            type: 'Array',
+            value: [
+              { type: 'String', value: this.accounts.node1.addr },
+            ],
+          },
+          _9: {
+            type: 'Array',
+            value: [
+              { type: 'Int', value: 100 },
+            ],
+          },
+          _10: { type: 'Int', value: 0 },
+        },
+      });
+    },
+  );
+
+  it(
+    'getTotalAssetsREADONLY should return correct values',
+    async function () {
+      const expr = 'getTotalAssetsREADONLY()';
+      const response = await api.utils.fetchEvaluate(
+        this.accounts.l2mpStaking.addr,
+        expr,
+      );
+      const checkData = response.result.value._2;
+
+      expect(checkData).to.eql({
+        type: 'Tuple',
+        value: {
+          _1: { type: 'Int', value: stakeAmount1 + stakeAmount2 + lockedAmount1 },
+          _2: { type: 'Int', value: stakeAmount1 + stakeAmount2 + lockedAmount1 },
+          _3: { type: 'BigInt', value: `${10e17}` },
+          _4: { type: 'Int', value: lockedAmount1 },
+          _5: { type: 'Int', value: lockedAmount1 },
+          _6: { type: 'Int', value: 0 },
+        },
+      });
+    },
+  );
+});
